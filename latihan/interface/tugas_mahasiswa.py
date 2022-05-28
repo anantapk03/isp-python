fa8e428c903476f02bc76bb1ed4094242609c3ea
anantapk03 <anantapk03@gmail.com>
2022-05-28 13:33:52 +0700

    Folder Interface By ANANTA PK

---
+++ latihan/interface/tugas_mahasiswa.py
@@ -0,0 +1,13 @@
+from interface.kehadiran import Kehadiran
+from abc import ABC , abstractmethod
+
+class TugasMahasiswa (Kehadiran, ABC) :
+    @abstractmethod
+    def mencatat_kehadiran (self) -> None :
+        super().mencatat_kehadiran()
+    
+    @abstractmethod
+    def mengerjakan_ujian (self) -> None :
+        pass 
+    
+    
